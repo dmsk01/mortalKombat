b5dce067a603c979fa9146665773367fbd33e057
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2021-04-24 14:21:13 +0300

    homework8

---
+++ game.js
@@ -0,0 +1,150 @@
+import { getRandom, getTime } from "./utils.js";
+export default class Game {
+  constructor(props) {
+    this.player1 = props.player1;
+    this.player2 = props.player2;
+    this.formFight = props.formFight;
+    this.arenas = document.querySelector(".arenas");
+    this.randomButton = document.querySelector(".button");
+    this.chat = document.querySelector(".chat");
+    this.logs = {
+      start: "Часы показывали [time], когда [player1] и [player2] бросили вызов друг другу.",
+      end: ["Результат удара [playerWins]: [playerLose] - труп", "[playerLose] погиб от удара бойца [playerWins]", "Результат боя: [playerLose] - жертва, [playerWins] - убийца"],
+      hit: [
+        "[playerDefence] пытался сконцентрироваться, но [playerKick] разбежавшись раздробил копчиком левое ухо врага.",
+        "[playerDefence] расстроился, как вдруг, неожиданно [playerKick] случайно раздробил грудью грудину противника.",
+        "[playerDefence] зажмурился, а в это время [playerKick], прослезившись, раздробил кулаком пах оппонента.",
+        "[playerDefence] чесал <вырезано цензурой>, и внезапно неустрашимый [playerKick] отчаянно размозжил грудью левый бицепс оппонента.",
+        "[playerDefence] задумался, но внезапно [playerKick] случайно влепил грубый удар копчиком в пояс оппонента.",
+        "[playerDefence] ковырялся в зубах, но [playerKick] проснувшись влепил тяжелый удар пальцем в кадык врага.",
+        "[playerDefence] вспомнил что-то важное, но внезапно [playerKick] зевнув, размозжил открытой ладонью челюсть противника.",
+        "[playerDefence] осмотрелся, и в это время [playerKick] мимоходом раздробил стопой аппендикс соперника.",
+        "[playerDefence] кашлянул, но внезапно [playerKick] показав палец, размозжил пальцем грудь соперника.",
+        "[playerDefence] пытался что-то сказать, а жестокий [playerKick] проснувшись размозжил копчиком левую ногу противника.",
+        "[playerDefence] забылся, как внезапно безумный [playerKick] со скуки, влепил удар коленом в левый бок соперника.",
+        "[playerDefence] поперхнулся, а за это [playerKick] мимоходом раздробил коленом висок врага.",
+        "[playerDefence] расстроился, а в это время наглый [playerKick] пошатнувшись размозжил копчиком губы оппонента.",
+        "[playerDefence] осмотрелся, но внезапно [playerKick] робко размозжил коленом левый глаз противника.",
+        "[playerDefence] осмотрелся, а [playerKick] вломил дробящий удар плечом, пробив блок, куда обычно не бьют оппонента.",
+        "[playerDefence] ковырялся в зубах, как вдруг, неожиданно [playerKick] отчаянно размозжил плечом мышцы пресса оппонента.",
+        "[playerDefence] пришел в себя, и в это время [playerKick] провел разбивающий удар кистью руки, пробив блок, в голень противника.",
+        "[playerDefence] пошатнулся, а в это время [playerKick] хихикая влепил грубый удар открытой ладонью по бедрам врага.",
+      ],
+      defence: [
+        "[playerKick] потерял момент и храбрый [playerDefence] отпрыгнул от удара открытой ладонью в ключицу.",
+        "[playerKick] не контролировал ситуацию, и потому [playerDefence] поставил блок на удар пяткой в правую грудь.",
+        "[playerKick] потерял момент и [playerDefence] поставил блок на удар коленом по селезенке.",
+        "[playerKick] поскользнулся и задумчивый [playerDefence] поставил блок на тычок головой в бровь.",
+        "[playerKick] старался провести удар, но непобедимый [playerDefence] ушел в сторону от удара копчиком прямо в пятку.",
+        "[playerKick] обманулся и жестокий [playerDefence] блокировал удар стопой в солнечное сплетение.",
+        "[playerKick] не думал о бое, потому расстроенный [playerDefence] отпрыгнул от удара кулаком куда обычно не бьют.",
+        "[playerKick] обманулся и жестокий [playerDefence] блокировал удар стопой в солнечное сплетение.",
+      ],
+      draw: "Ничья - это тоже победа!",
+    };
+  }
+  createElement = (tagName, className) => {
+    const tag = document.createElement(tagName);
+    if (className) {
+      tag.classList.add(className);
+    }
+    return tag;
+  };
+  createPlayer = ({ player, img, hp, name }) => {
+    const playerContainer = this.createElement("div", `player${player}`);
+
+    const progressbar = this.createElement("div", "progressbar");
+
+    const character = this.createElement("div", "character");
+
+    const characterImage = this.createElement("img");
+    characterImage.src = img;
+
+    character.appendChild(characterImage);
+
+    playerContainer.appendChild(progressbar);
+    playerContainer.appendChild(character);
+
+    const life = this.createElement("div", "life");
+
+    life.style.width = `${hp}%`;
+
+    const playerName = this.createElement("div", "name");
+    playerName.innerText = name;
+
+    progressbar.appendChild(life);
+    progressbar.appendChild(playerName);
+
+    return playerContainer;
+  };
+  createReloadButton = () => {
+    const buttonWrap = this.createElement("div", "reloadWrap");
+
+    const button = this.createElement("button", "button");
+
+    button.innerText = "Restart";
+
+    button.addEventListener("click", function () {
+      window.location.reload();
+    });
+
+    buttonWrap.appendChild(button);
+
+    this.arenas.appendChild(buttonWrap);
+  };
+  generateLogs = (type, { name } = {}, { name: player2Name, hp } = {}, playerHp) => {
+    let text;
+    switch (type) {
+      case "start":
+        text = this.logs[type].replace("[time]", getTime()).replace("[player1]", name).replace("[player2]", player2Name);
+        break;
+      case "hit":
+        text = this.logs[type][getRandom(this.logs.hit.length - 1) - 1].replace("[playerKick]", name).replace("[playerDefence]", player2Name) + `, -${playerHp}HP, [${hp}/100]`;
+        break;
+      case "defence":
+        text = this.logs[type][getRandom(this.logs.defence.length - 1) - 1].replace("[playerKick]", player2Name).replace("[playerDefence]", name);
+        break;
+      case "draw":
+        text = this.logs[type];
+        break;
+      case "end":
+        text = this.logs[type][getRandom(this.logs.end.length - 1) - 1].replace("[playerWins]", name).replace("[playerLose]", player2Name);
+        break;
+      default:
+        text = "Something went wrong!";
+    }
+    const el = `<p>${getTime()} ${text}</p>`;
+    this.chat.insertAdjacentHTML("afterbegin", el);
+  };
+  playerWins = (name) => {
+    const winTitle = this.createElement("div", "loseTitle");
+    if (name) {
+      winTitle.innerText = `${name} wins!`;
+    } else {
+      winTitle.innerText = `Draw!`;
+    }
+    return winTitle;
+  };
+  showResult = (player1, player2) => {
+    if (player1.hp === 0 || player2.hp === 0) {
+      this.randomButton.disabled = true;
+      this.createReloadButton();
+    }
+
+    if (player1.hp === 0 && player1.hp < player2.hp) {
+      this.arenas.appendChild(this.playerWins(player2.name));
+      this.generateLogs("end", player2, player1);
+    } else if (player2.hp === 0 && player1.hp > player2.hp) {
+      this.arenas.appendChild(this.playerWins(player1.name));
+      this.generateLogs("end", player1, player2);
+    } else if (player1.hp === 0 && player2.hp === 0) {
+      this.arenas.appendChild(this.playerWins());
+      this.generateLogs("draw");
+    }
+  };
+  start = () => {
+    this.arenas.appendChild(this.createPlayer(this.player1));
+    this.arenas.appendChild(this.createPlayer(this.player2));
+    this.generateLogs("start", this.player1, this.player2);
+  };
+}
--- player.js
+++ player.js
@@ -1,84 +1,87 @@
 import { getRandom } from "./utils.js";
 
-export const player1 = {
+class Player {
+  constructor(props) {
+    this.player = props.player;
+    this.name = props.name;
+    this.hp = props.hp;
+    this.img = props.img;
+    this.HIT = {
+      head: 30,
+      body: 25,
+      foot: 20,
+    };
+
+    this.ATTACK = ["head", "body", "foot"];
+  }
+  attack = () => {
+    return `${this.name} Fight...`;
+  };
+
+  changeHP = (num) => {
+    this.hp -= num;
+
+    if (this.hp <= 0) {
+      this.hp = 0;
+    }
+  };
+
+  elHP = () => {
+    return document.querySelector(`.player${this.player} .life`);
+  };
+
+  renderHP = (elHP) => {
+    elHP.style.width = this.hp + "%";
+  };
+}
+class Player1 extends Player {
+  constructor(props) {
+    super(props);
+  }
+  playerAttack = (formFight) => {
+    const attack = {};
+
+    for (let item of formFight) {
+      if (item.checked && item.name === "hit") {
+        attack.value = getRandom(this.HIT[item.value]);
+        attack.hit = item.value;
+      }
+
+      if (item.checked && item.name === "defence") {
+        attack.defence = item.value;
+      }
+
+      item.checked = false;
+    }
+
+    return attack;
+  };
+}
+class Player2 extends Player {
+  constructor(props) {
+    super(props);
+  }
+  enemyAttack = () => {
+    const hit = this.ATTACK[getRandom(3) - 1];
+    const defence = this.ATTACK[getRandom(3) - 1];
+
+    return {
+      value: getRandom(this.HIT[hit]),
+      hit,
+      defence,
+    };
+  };
+}
+
+export const player1 = new Player1({
   player: 1,
   name: "sonya",
   hp: 100,
   img: "http://reactmarathon-api.herokuapp.com/assets/sonya.gif",
-  weapon: ["Ssanaya tryapka"],
-  attack,
-  changeHP,
-  elHP,
-  renderHP,
-};
-
-export const player2 = {
+});
+export const player2 = new Player2({
   player: 2,
   name: "subzero",
   hp: 100,
   img: "http://reactmarathon-api.herokuapp.com/assets/subzero.gif",
-  weapon: ["Ssanyi venik"],
-  attack,
-  changeHP,
-  elHP,
-  renderHP,
-};
-
-const HIT = {
-  head: 30,
-  body: 25,
-  foot: 20,
-};
-
-const ATTACK = ["head", "body", "foot"];
-
-function attack() {
-  return `${this.name} Fight...`;
-}
-
-function changeHP(num) {
-  this.hp -= num;
-
-  if (this.hp <= 0) {
-    this.hp = 0;
-  }
-}
-
-function elHP() {
-  return document.querySelector(`.player${this.player} .life`);
-}
-
-function renderHP(elHP) {
-  elHP.style.width = this.hp + "%";
-}
-
-export function enemyAttack() {
-  const hit = ATTACK[getRandom(3) - 1];
-  const defence = ATTACK[getRandom(3) - 1];
-
-  return {
-    value: getRandom(HIT[hit]),
-    hit,
-    defence,
-  };
-}
-
-export function playerAttack(formFight) {
-  const attack = {};
-
-  for (let item of formFight) {
-    if (item.checked && item.name === "hit") {
-      attack.value = getRandom(HIT[item.value]);
-      attack.hit = item.value;
-    }
-
-    if (item.checked && item.name === "defence") {
-      attack.defence = item.value;
-    }
-
-    item.checked = false;
-  }
-
-  return attack;
-}
-
+});
